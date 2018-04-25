917c1bfe73aef573937d590c4dc5548c3a362e17
zhanzongyuan <zhanzy5@mail2.sysu.edu.cn>
2018-04-25 23:08:00 +0800

    Update auto_config.sh

--- auto_config.sh
+++ auto_config.sh
@@ -1,9 +1,17 @@
 # Install necessary softwares and make linux server(Ubuntu or Debian) profile.
+
+# update apt-get
 sudo apt-get update
+
+# git
 sudo apt-get install git -y
-git clone https://github.com/zhanzongyuan/linux-profile.git
-cat ./linux-profile/.bashrc>> ~/.bashrc
+git clone https://github.com/zhanzongyuan/linux-configuration.git
+cat ./linux-configuration/.bashrc>> ~/.bashrc
+
+# vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-cp ./linux-profile/.vimrc ~/.vimrc
+cp ./linux-configuration/.vimrc ~/.vimrc
+
+# tmux
 sudo apt-get install tmux -y
