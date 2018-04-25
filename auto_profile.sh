3834e8eb3ce3ce4b3a40a9d64c749e3f208be9ef
zhanzongyuan <zhanzy5@mail2.sysu.edu.cn>
2018-04-25 22:52:32 +0800

    Update auto_profile.sh

--- auto_profile.sh
+++ auto_profile.sh
@@ -3,5 +3,7 @@ sudo apt-get update
 sudo apt-get install git -y
 git clone https://github.com/zhanzongyuan/linux-profile.git
 cat ./linux-profile/.bashrc>> ~/.bashrc
-
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+cp ./linux-profile/.vimrc ~/.vimrc
 
