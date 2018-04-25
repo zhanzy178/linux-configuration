a2c4ba08cabf891d86d82fda6af19ff21733ef45
zhanzongyuan <zhanzy5@mail2.sysu.edu.cn>
2018-04-25 22:51:36 +0800

    Update auto_profile.sh

--- auto_profile.sh
+++ auto_profile.sh
@@ -1,2 +1,7 @@
 # Install necessary softwares and make linux server(Ubuntu or Debian) profile.
 sudo apt-get update
+sudo apt-get install git -y
+git clone https://github.com/zhanzongyuan/linux-profile.git
+cat ./linux-profile/.bashrc>> ~/.bashrc
+
+
